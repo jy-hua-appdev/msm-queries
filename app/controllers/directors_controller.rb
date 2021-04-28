2851deba05039d5c786dd93fee3870ff34871953
jy-hua <jhuang17@chicagobooth.edu>
2021-04-28 20:21:48 +0000

    directors partially working, not movies by director yet

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -7,7 +7,25 @@ class DirectorsController < ApplicationController
   end
 
   def eldest
+    @oldest_director_name = Director.where.not({ :dob => nil}).order({ :dob => :asc }).at(0).name
+    @oldest_director_dob = Director.where.not({ :dob => nil}).order({ :dob => :asc }).at(0).dob
+    @oldest_director_id = Director.where.not({ :dob => nil}).order({ :dob => :asc }).at(0).id
     render({ :template => "director_templates/eldest.html.erb"})
+  end
+
+  def youngest
+    @youngest_director_name = Director.where.not({ :dob => nil}).order({ :dob => :desc }).at(0).name
+    @youngest_director_dob = Director.where.not({ :dob => nil}).order({ :dob => :desc }).at(0).dob
+    @youngest_director_id = Director.where.not({ :dob => nil}).order({ :dob => :desc }).at(0).id
+    render({ :template => "director_templates/youngest.html.erb"})
+  end
+
+  def director_details
+    the_id = params.fetch("an_id")
+
+    @the_director = Director.where({ :id => the_id }).first
+
+    render({ :template => "director_templates/director_details.html.erb"})
   end
 
 end
